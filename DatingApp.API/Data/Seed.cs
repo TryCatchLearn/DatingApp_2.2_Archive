4aefafcc9ce874589e29a30335ecd2ab5b530c5d
trycatchlearn <trycatchlearn@outlook.com>
2018-08-07 12:04:52 +0100

    Identity seed user fix

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -10,20 +10,20 @@ namespace DatingApp.API.Data
     {
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<Role> _roleManager;
+
         public Seed(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
-            _roleManager = roleManager;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
-        public async void SeedUsers()
+        public void SeedUsers()
         {
             if (!_userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
 
-                // create some roles
                 var roles = new List<Role>
                 {
                     new Role{Name = "Member"},
@@ -33,28 +33,27 @@ namespace DatingApp.API.Data
                 };
 
                 foreach (var role in roles)
-                {   
-                    await _roleManager.CreateAsync(role);
+                {
+                    _roleManager.CreateAsync(role).Wait();
                 }
 
                 foreach (var user in users)
                 {
-                    await _userManager.CreateAsync(user, "password");
-                    await _userManager.AddToRoleAsync(user, "Member");
+                    _userManager.CreateAsync(user, "password").Wait();
+                    _userManager.AddToRoleAsync(user, "Member").Wait();
                 }
 
-                // create admin user
                 var adminUser = new User
                 {
                     UserName = "Admin"
                 };
 
-                var result = await _userManager.CreateAsync(adminUser, "password");
+                IdentityResult result = _userManager.CreateAsync(adminUser, "password").Result;
 
                 if (result.Succeeded)
                 {
-                    var admin = await _userManager.FindByNameAsync("Admin");
-                    await _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"});
+                    var admin = _userManager.FindByNameAsync("Admin").Result;
+                    _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"}).Wait();
                 }
             }
         }
